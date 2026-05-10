7b6a5496f212b2c101ef8605f2d62eb091ec4c9a
sean <lucky.mr.sean@gmail.com>
2026-05-10 10:59:36 +0000

    test: wire interactive-pty suite into run-all-tests.sh

--- autoresearch/scripts/run-all-tests.sh
+++ autoresearch/scripts/run-all-tests.sh
@@ -22,6 +22,7 @@ run_suite() {
 
 run_suite "M2  logger"   "tsx scripts/test-log.ts"
 run_suite "M1  terminal" "tsx scripts/test-term.ts"
+run_suite "M3  interactive-pty" "tsx scripts/test-interactive-pty.ts"
 run_suite "M4  errors"   "tsx scripts/test-errors.ts"
 
 echo ""
